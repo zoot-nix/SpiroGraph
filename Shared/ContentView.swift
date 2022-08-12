a8992586ca69f85399b1d1fbfe42613bc8460905
Owais Shaikh <theowaiszoot@gmail.com>
2022-08-12 20:46:08 +0530

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Owais Shaikh on 12/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/SpiroGraphApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpiroGraphApp.swift
+//  Shared
+//
+//  Created by Owais Shaikh on 12/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SpiroGraphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
